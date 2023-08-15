09ac23b9df17a33c7f443ce06972b0317319b8d1
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2023-08-15 15:17:21 +0700

    Add Clerk (authentications) and create NavBar components

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="flex justify-between p-3 px-10 border-b-[1px] shadow-sm">
+      <div className="flex gap-10 items-center">
+        <Image src="/logo.png" alt="logo" width={120} height={60} />
+        <div className="hidden md:flex gap-6">
+          <h2>Home</h2>
+          <h2>History</h2>
+          <h2>Help</h2>
+        </div>
+      </div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  );
+}
+
+export default NavBar;
