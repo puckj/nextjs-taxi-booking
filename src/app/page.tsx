6889233257891bcdadea1e815b6de4193e0144cd
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2023-08-17 15:35:40 +0700

    get user geolocation and add marker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,35 @@
+"use client";
 import Booking from "@/components/Booking/Booking";
 import MapBoxMap from "@/components/Map/MapBoxMap";
+import { UserLocationContext } from "@/context/UserLocationContext";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [userLocation, setUserLocation] = useState<{
+    lat: number | null;
+    lng: number | null;
+  }>({ lat: null, lng: null });
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      setUserLocation({ lat: latitude, lng: longitude });
+    });
+  };
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-3">
-        <Booking />
-        <div className="col-span-2">
-          <MapBoxMap />
+      <UserLocationContext.Provider value={{ userLocation, setUserLocation }}>
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <Booking />
+          <div className="col-span-2">
+            <MapBoxMap />
+          </div>
         </div>
-      </div>
+      </UserLocationContext.Provider>
     </div>
   );
 }
